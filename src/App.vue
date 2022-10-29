70adcfab17384e1f0e6e772c490064b99bdbd8c3
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2022-10-29 19:39:06 +0900

    라우터 세팅 완료

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
+    <ToolBar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
 
 export default {
   name: 'App',
+  components: {
+    ToolBar,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
 </style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,18 @@ import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import AskView from "../views/AskView.vue";
 import JobsView from "../views/JobsView.vue";
+import UserView from "../views/UserView.vue"
+import ItemView from "../views/ItemView.vue"
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+    mode: 'history',
     routes: [
+        {
+            path: '/',
+            redirect: '/news',
+        },
         {
             // path: url에 대한 정보
             path: '/news',
@@ -21,6 +28,14 @@ export const router = new VueRouter({
         {
             path: '/jobs',
             component: JobsView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
         },
     ]
 });
